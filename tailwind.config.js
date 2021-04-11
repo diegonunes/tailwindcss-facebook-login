344b5a964eae3112127b81cab1ed5120908b69a6
Diego Nunes <diegonunesdev@gmail.com>
2021-04-11 01:14:42 -0300

    🎉 initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      spacing: {
+        30: '7.5rem',
+        '290px': '290px',
+        '396px': '396px',
+        '500px': '500px',
+        '980px': '980px',
+      },
+      fontSize: {
+        subtitle: '1.7rem',
+      },
+      colors: {
+        'main-gray': '#f0f2f5',
+        'main-blue': '#1877f2',
+        'main-blue-hover': '#166fe5',
+        'main-green': '#42b72a',
+        'main-green-hover': '#36a420',
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
